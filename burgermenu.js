87dc86649fa95cb844ceaad9052bf5de12c5cbda
Luka <lukhanyo95@gmail.com>
2021-07-22 13:25:26 +0200

    reworked portfoio

--- burgermenu.js
+++
@@ -1,7 +0,0 @@
-const burger = document.getElementById('burger_menu');
-const navUl = document.querySelector('.nav_list');
-
-burger.addEventListener('click', () =>{
-    navUl.classList.toggle('show');
-    burger.classList.toggle('change');
-})
